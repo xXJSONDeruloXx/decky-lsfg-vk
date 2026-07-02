d3124250b8d2252aeb0f9b917efd8b78f13a264d
Ibrahim Ahmed <97i.a0011@gmail.com>
2026-07-02 18:04:34 +0300

    paths.py

---
+++ py_modules/lsfg_vk/paths.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+HOME = Path.home()
+
+CONFIG_DIR = HOME / ".config" / "lsfg-vk"
+
+FEX_CONFIG = HOME / ".config" / "fex-emu" / "Config.json"
+
+STEAM_DIR = HOME / ".local/share/Steam"
+
+LIB_DIR = HOME / ".local/lib"
+
+VULKAN_DIR = HOME / ".local/share/vulkan/implicit_layer.d"
