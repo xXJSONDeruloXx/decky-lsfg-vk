644e624e6e8745f58b4f3bee4a672f180992ad1b
Kurt Himebauch <136133082+xXJSONDeruloXx@users.noreply.github.com>
2026-09-10 15:46:18 -0400

    feat: add flatpak setup to setup tab

--- src/components/SetupTab.tsx
+++ src/components/SetupTab.tsx
@@ -1,6 +1,7 @@
 import { ButtonItem, Field, PanelSection, PanelSectionRow } from "@decky/ui";
 import { type SteamBranchStatus } from "../api/lsfgApi";
 import t from "../i18n/i18n";
+import { FlatpakSetupSection } from "./FlatpakSetupSection";
 
 interface SetupTabProps {
   isInstalled: boolean;
@@ -36,33 +37,36 @@ export function SetupTab(props: SetupTabProps) {
         : t("INSTALL_INSTALL_BTN", "Install LSFG-VK");
 
   return (
-    <PanelSection title="Setup">
-      <PanelSectionRow>
-        <Field
-          label="Lossless Scaling"
-          description={losslessScalingAppInstalled ? "Installed" : losslessScalingStatus || "Not installed"}
-        />
-      </PanelSectionRow>
-      <PanelSectionRow>
-        <Field label="LSFG-VK" description={installationStatus} />
-      </PanelSectionRow>
-      {steamBranchStatus?.installed && (
+    <>
+      <PanelSection title="Setup">
         <PanelSectionRow>
           <Field
-            label="Steam branch"
-            description={`${steamBranchStatus.current_branch || "public"}${steamBranchStatus.needs_switch ? ` - ${steamBranchStatus.message}` : ""}`}
+            label="Lossless Scaling"
+            description={losslessScalingAppInstalled ? "Installed" : losslessScalingStatus || "Not installed"}
           />
         </PanelSectionRow>
-      )}
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={isInstalled ? onUninstall : onInstall}
-          disabled={isInstalling || isUninstalling}
-        >
-          {buttonLabel}
-        </ButtonItem>
-      </PanelSectionRow>
-    </PanelSection>
+        <PanelSectionRow>
+          <Field label="LSFG-VK" description={installationStatus} />
+        </PanelSectionRow>
+        {steamBranchStatus?.installed && (
+          <PanelSectionRow>
+            <Field
+              label="Steam branch"
+              description={`${steamBranchStatus.current_branch || "public"}${steamBranchStatus.needs_switch ? ` - ${steamBranchStatus.message}` : ""}`}
+            />
+          </PanelSectionRow>
+        )}
+        <PanelSectionRow>
+          <ButtonItem
+            layout="below"
+            onClick={isInstalled ? onUninstall : onInstall}
+            disabled={isInstalling || isUninstalling}
+          >
+            {buttonLabel}
+          </ButtonItem>
+        </PanelSectionRow>
+      </PanelSection>
+      <FlatpakSetupSection enabled={isInstalled} />
+    </>
   );
 }
